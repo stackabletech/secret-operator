1cd00bdef507a8db3acb27bea70c14154168f862
Teo Klestrup Röijezon <teo@nullable.se>
2022-01-18 12:03:38 +0100

    Print the typical startup banner on startup

--- build.rs
+++ build.rs
@@ -8,4 +8,12 @@ fn main() {
         .file_descriptor_set_path(out_dir.join("file_descriptor_set.bin"))
         .compile(&["csi.proto"], &["vendor/csi"])
         .unwrap();
+    built::write_built_file_with_opts(
+        // built's env module depends on a whole bunch of variables that crate2nix doesn't provide
+        // so we grab the specific env variables that we care about out ourselves instead.
+        built::Options::default().set_env(false),
+        "Cargo.toml".as_ref(),
+        &out_dir.join("built.rs"),
+    )
+    .unwrap();
 }
