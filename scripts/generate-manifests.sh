bfe78fa3242772a7d439f28012d4c2979ba12db9
Stacky McStackface <github@stackable.de>
2022-04-01 19:40:23 +0000

    Generated commit to update templated files based on rev 8135e36 in stackabletech/operator-templating repo.
    Triggered by:
    Manual run triggered by: soenkeliebau with message [Rerun after fixing accidental hard coded value]

--- scripts/generate-manifests.sh
+++ scripts/generate-manifests.sh
@@ -1,22 +1,21 @@
-#!/bin/bash
-# This script reads a Helm chart from deploy/helm/secret-operator-operator and
+#!/usr/bin/env bash
+# This script reads a Helm chart from deploy/helm/secret-operator and
 # generates manifest files into deploy/manifestss
 set -e
 
 tmp=$(mktemp -d ./manifests-XXXXX)
 
-helm template --output-dir $tmp \
+helm template --output-dir "$tmp" \
               --include-crds \
               --name-template secret-operator \
               deploy/helm/secret-operator
 
-for file in $(find $tmp -type f)
-do
-    yq eval -i 'del(.. | select(has("app.kubernetes.io/managed-by")) | ."app.kubernetes.io/managed-by")' $file
-    yq eval -i 'del(.. | select(has("helm.sh/chart")) | ."helm.sh/chart")' $file
-    sed -i '/# Source: .*/d' $file
+for file in $tmp/secret-operator/*/*; do
+  yq eval -i 'del(.. | select(has("app.kubernetes.io/managed-by")) | ."app.kubernetes.io/managed-by")' /dev/stdin < "$file"
+  yq eval -i 'del(.. | select(has("helm.sh/chart")) | ."helm.sh/chart")' /dev/stdin < "$file"
+  sed -i '/# Source: .*/d' "$file"
 done
 
-cp -r $tmp/secret-operator/*/* deploy/manifests/
+cp -r "$tmp"/secret-operator/*/* deploy/manifests/
 
-rm -rf $tmp
+rm -rf "$tmp"
