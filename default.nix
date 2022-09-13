2b73b9e42a1d299d3d1138bfdaa1a5014d7aa7f7
Teo Klestrup Röijezon <teo@nullable.se>
2022-09-13 07:16:48 +0000

    Update Nix build instructions (#181)
    Backport of https://github.com/stackabletech/listener-operator/commit/654e79a610d42394eccb061082f3c950c78beb96
    
    
    # Description
    
    *Please add a description here. This will become the commit message of the merge request later.*

--- default.nix
+++ default.nix
@@ -40,6 +40,14 @@ rec {
       path = pkgs.writeText "${dockerImage.name}-image-tag" "${dockerImage.imageName}:${dockerImage.imageTag}";
     }
     {
+      name = "image-repo";
+      path = pkgs.writeText "${dockerImage.name}-repo" dockerImage.imageName;
+    }
+    {
+      name = "image-tag";
+      path = pkgs.writeText "${dockerImage.name}-tag" dockerImage.imageTag;
+    }
+    {
       name = "crds.yaml";
       path = crds;
     }
