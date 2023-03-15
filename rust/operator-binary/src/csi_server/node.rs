dba41b96054dc096ab35bad6c1e5da1704f6b84d
Natalie Klestrup Röijezon <nat@nullable.se>
2023-03-15 14:17:50 +0100

    Implement unprivileged mode

--- rust/operator-binary/src/csi_server/node.rs
+++ rust/operator-binary/src/csi_server/node.rs
@@ -134,6 +134,7 @@ impl From<UnpublishError> for Status {
 pub struct SecretProvisionerNode {
     pub client: stackable_operator::client::Client,
     pub node_name: String,
+    pub privileged: bool,
 }
 
 impl SecretProvisionerNode {
@@ -158,14 +159,18 @@ impl SecretProvisionerNode {
                 _ => return Err(err).context(publish_error::CreateDirSnafu { path: target_path }),
             },
         }
-        Mount::new(
-            "",
-            target_path,
-            "tmpfs",
-            MountFlags::NODEV | MountFlags::NOEXEC | MountFlags::NOSUID,
-            None,
-        )
-        .context(publish_error::MountSnafu { path: target_path })?;
+        if self.privileged {
+            Mount::new(
+                "",
+                target_path,
+                "tmpfs",
+                MountFlags::NODEV | MountFlags::NOEXEC | MountFlags::NOSUID,
+                None,
+            )
+            .context(publish_error::MountSnafu { path: target_path })?;
+        } else {
+            tracing::info!("running in unprivileged mode, not creating mount for secret volume");
+        }
         // User: root/secret-operator
         // Group: Controlled by Pod.securityContext.fsGroup, the actual application
         // (when running as unprivileged user)
@@ -260,23 +265,37 @@ impl SecretProvisionerNode {
     }
 
     async fn clean_secret_dir(&self, target_path: &Path) -> Result<(), UnpublishError> {
-        match unmount(target_path, UnmountFlags::empty()) {
-            Ok(_) => {}
-            Err(err) => match err.kind() {
-                std::io::ErrorKind::NotFound => {
-                    tracing::warn!(volume.path = %target_path.display(), "Tried to delete volume path that does not exist, assuming it was already deleted");
-                    return Ok(());
-                }
-                std::io::ErrorKind::InvalidInput => {
-                    tracing::warn!(volume.path = %target_path.display(), "Tried to unmount volume path that is not mounted, trying to delete it anyway");
-                }
-                _ => return Err(err).context(unpublish_error::UnmountSnafu { path: target_path }),
-            },
-        };
-        tokio::fs::remove_dir(&target_path)
-            .await
-            .context(unpublish_error::DeleteSnafu { path: target_path })?;
-        Ok(())
+        // unmount() fails unconditionally with PermissionDenied when running in an unprivileged container,
+        // even if it wouldn't be sensible to even try anyway (such as when there is no volume mount).
+        if self.privileged {
+            match unmount(target_path, UnmountFlags::empty()) {
+                Ok(_) => {}
+                Err(err) => match err.kind() {
+                    std::io::ErrorKind::NotFound => {
+                        tracing::warn!(volume.path = %target_path.display(), "Tried to unmount volume path that does not exist, assuming it was already deleted");
+                        return Ok(());
+                    }
+                    std::io::ErrorKind::InvalidInput => {
+                        tracing::warn!(volume.path = %target_path.display(), "Tried to unmount volume path that is not mounted, trying to delete it anyway");
+                    }
+                    _ => {
+                        return Err(err)
+                            .context(unpublish_error::UnmountSnafu { path: target_path })
+                    }
+                },
+            };
+        }
+        // There is no mount in unprivileged mode, so we need to remove all contents in that case.
+        // This may still apply to privileged mode, in case users are migrating from unprivileged to privileged mode.
+        match tokio::fs::remove_dir_all(&target_path).await {
+            Ok(_) => Ok(()),
+            // We already catch this above when running in privileged mode, but in unprivileged mode this is still possible
+            Err(err) if err.kind() == std::io::ErrorKind::NotFound => {
+                tracing::warn!(volume.path = %target_path.display(), "Tried to delete volume path that does not exist, assuming it was already deleted");
+                Ok(())
+            }
+            Err(err) => Err(err).context(unpublish_error::DeleteSnafu { path: target_path }),
+        }
     }
 }
 
