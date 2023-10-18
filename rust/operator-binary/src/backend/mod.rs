e846eed66e4bbbf9d737747677743979e11fa569
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2023-10-18 11:23:10 +0200

    link to concepts page

--- rust/operator-binary/src/backend/mod.rs
+++ rust/operator-binary/src/backend/mod.rs
@@ -89,7 +89,8 @@ pub struct SecretVolumeSelector {
     )]
     pub compat_tls_pkcs12_password: Option<String>,
 
-    /// The TLS cert lifetime (`24h`, `1d` or `7d`).
+    /// The TLS cert lifetime.
+    /// The format is documented in https://docs.stackable.tech/home/nightly/concepts/duration.
     #[serde(
         rename = "secrets.stackable.tech/backend.autotls.cert.lifetime",
         default = "default_cert_lifetime"
@@ -100,8 +101,7 @@ pub struct SecretVolumeSelector {
     /// Keep in mind that there can be multiple Pods - such as 80 datanodes - trying to
     /// shut down at the same time. It can take some hours until all Pods are restarted
     /// in a rolling fashion.
-    ///
-    /// The format is `1d2h3m4s` (components are optional, `1m` is also a valid value).
+    /// The format is documented in https://docs.stackable.tech/home/nightly/concepts/duration.
     #[serde(
         rename = "secrets.stackable.tech/backend.autotls.cert.restart-buffer",
         default = "default_cert_restart_buffer"
