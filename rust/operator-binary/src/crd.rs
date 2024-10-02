5873540f65c279bdd1131f864f0aa8edfe10b3d8
Malte Sander <malte.sander.it@gmail.com>
2024-10-02 09:53:24 +0200

    Update rust/operator-binary/src/crd.rs
    Co-authored-by: Sebastian Bernauer <sebastian.bernauer@stackable.de>

--- rust/operator-binary/src/crd.rs
+++ rust/operator-binary/src/crd.rs
@@ -140,6 +140,8 @@ impl AutoTlsCa {
 #[serde(rename_all = "camelCase")]
 pub enum TlsKeyGeneration {
     Rsa {
+        /// The amount of bits used for key or certs. Currently, `2048`, `3072` and `4096` are
+        /// supported. Defaults to `2048` bits.
         #[schemars(schema_with = "TlsKeyGeneration::tls_key_length_schema")]
         length: u32,
     },
