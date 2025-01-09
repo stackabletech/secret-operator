fa1166830b2c9a9e10f6207927012b81f140c904
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2025-01-09 13:20:42 +0100

    wip

--- rust/olm-deployer/src/main.rs
+++ rust/olm-deployer/src/main.rs
@@ -1,3 +1,9 @@
+// TODO:
+//
+// - get the deployment object for the olm helper
+// - get the env, tolerations, etc from the deployment
+// - filter out the secret daemonset from the given manifest files
+// - add env, tolerations, etc to the daemonset
 use anyhow::{bail, Context, Result};
 use clap::{crate_description, crate_version, Parser};
 use stackable_operator::cli::Command;
@@ -27,6 +33,8 @@ struct Opts {
 #[derive(clap::Parser)]
 struct OlmDeployerRun {
     #[arg(long, short)]
+    namespace: String,
+    #[arg(long, short)]
     dir: std::path::PathBuf,
     /// Tracing log collector system
     #[arg(long, env, default_value_t, value_enum)]
@@ -39,6 +47,7 @@ struct OlmDeployerRun {
 async fn main() -> Result<()> {
     let opts = Opts::parse();
     if let Command::Run(OlmDeployerRun {
+        namespace,
         dir,
         tracing_target,
         cluster_info_opts,
@@ -119,4 +128,280 @@ fn multidoc_deserialize(data: &str) -> Result<Vec<serde_yaml::Value>> {
 
 fn dynamic_api(ar: ApiResource, client: &client::Client) -> Api<DynamicObject> {
     Api::default_namespaced_with(client.as_kube_client(), &ar)
+}
+
+#[cfg(test)]
+mod test {
+    use anyhow::Result;
+    use serde::Deserialize;
+    use stackable_operator::{
+        k8s_openapi::{
+            api::{
+                apps::v1::{DaemonSet, Deployment},
+                core::v1::Toleration,
+            },
+            Resource,
+        },
+        kube::api::DynamicObject,
+    };
+
+    const DAEMONSET: &str = r#"
+---
+apiVersion: apps/v1
+kind: DaemonSet
+metadata:
+  name: secret-operator-daemonset
+  labels:
+    app.kubernetes.io/instance: secret-operator
+    app.kubernetes.io/name: secret-operator
+    app.kubernetes.io/version: "24.11.0"
+    stackable.tech/vendor: Stackable
+spec:
+  selector:
+    matchLabels:
+      app.kubernetes.io/name: secret-operator
+      app.kubernetes.io/instance: secret-operator
+      stackable.tech/vendor: Stackable
+  template:
+    metadata:
+      labels:
+        app.kubernetes.io/name: secret-operator
+        app.kubernetes.io/instance: secret-operator
+        stackable.tech/vendor: Stackable
+    spec:
+      serviceAccountName: secret-operator-serviceaccount
+      securityContext: {}
+      containers:
+        - name: secret-operator
+          securityContext:
+            privileged: true
+            runAsUser: 0
+          image: "quay.io/stackable/secret-operator@sha256:bb5063aa67336465fd3fa80a7c6fd82ac6e30ebe3ffc6dba6ca84c1f1af95bfe"
+          imagePullPolicy: IfNotPresent
+          resources:
+            limits:
+              cpu: 100m
+              memory: 128Mi
+            requests:
+              cpu: 100m
+              memory: 128Mi
+          env:
+            - name: CSI_ENDPOINT
+              value: /csi/csi.sock
+            - name: NODE_NAME
+              valueFrom:
+                fieldRef:
+                  apiVersion: v1
+                  fieldPath: spec.nodeName
+            - name: PRIVILEGED
+              value: "true"
+          volumeMounts:
+            - name: csi
+              mountPath: /csi
+            - name: mountpoint
+              mountPath: /var/lib/kubelet/pods
+              mountPropagation: Bidirectional
+            - name: tmp
+              mountPath: /tmp
+        - name: external-provisioner
+          image: "quay.io/stackable/sig-storage/csi-provisioner@sha256:dd730457133f619d8759269abcfa79d7aeb817e01ba7af2d3aa1417df406ea56"
+          imagePullPolicy: IfNotPresent
+          resources:
+            limits:
+              cpu: 100m
+              memory: 128Mi
+            requests:
+              cpu: 100m
+              memory: 128Mi
+          args:
+            - --csi-address=/csi/csi.sock
+            - --feature-gates=Topology=true
+            - --extra-create-metadata
+          volumeMounts:
+            - name: csi
+              mountPath: /csi
+        - name: node-driver-registrar
+          image: "quay.io/stackable/sig-storage/csi-node-driver-registrar@sha256:8331d680e6c40c56909f436ea3126fdca39761cf87781cd2db8d05c9082b05f5"
+          imagePullPolicy: IfNotPresent
+          resources:
+            limits:
+              cpu: 100m
+              memory: 128Mi
+            requests:
+              cpu: 100m
+              memory: 128Mi
+          args:
+            - --csi-address=/csi/csi.sock
+            - --kubelet-registration-path=/var/lib/kubelet/plugins/secrets.stackable.tech/csi.sock
+          volumeMounts:
+            - name: registration-sock
+              mountPath: /registration
+            - name: csi
+              mountPath: /csi
+      initContainers:
+        - name: migrate-longer-csi-registration-path
+          image: "quay.io/stackable/secret-operator@sha256:bb5063aa67336465fd3fa80a7c6fd82ac6e30ebe3ffc6dba6ca84c1f1af95bfe"
+          imagePullPolicy: IfNotPresent
+          resources:
+            limits:
+              cpu: 100m
+              memory: 128Mi
+            requests:
+              cpu: 100m
+              memory: 128Mi
+          command:
+            - /bin/bash
+            - -euo
+            - pipefail
+            - -x
+            - -c
+            - |
+              ls -la /registration
+              echo "Removing old (long) CSI registration path"
+              if [ -d "/registration/secrets.stackable.tech-reg.sock" ]; then rmdir /registration/secrets.stackable.tech-reg.sock; fi
+              ls -la /registration
+          volumeMounts:
+            - name: registration-sock
+              mountPath: /registration
+          securityContext:
+            runAsUser: 0
+      volumes:
+        - name: registration-sock
+          hostPath:
+            # node-driver-registrar appends a driver-unique filename to this path to avoid conflicts
+            # see https://github.com/stackabletech/secret-operator/issues/229 for why this path should not be too long
+            path: /var/lib/kubelet/plugins_registry
+        - name: csi
+          hostPath:
+            path: /var/lib/kubelet/plugins/secrets.stackable.tech/
+        - name: mountpoint
+          hostPath:
+            path: /var/lib/kubelet/pods/
+        - name: tmp
+          emptyDir: {}
+"#;
+
+    const DEPLOYMENT: &str = r#"
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: secret-operator-deployer
+  labels:
+    app.kubernetes.io/name: secret-operator-deployer
+    app.kubernetes.io/instance: secret-operator-deployer
+    stackable.tech/vendor: Stackable
+spec:
+  replicas: 1
+  strategy:
+    type: Recreate
+  selector:
+    matchLabels:
+      app.kubernetes.io/name: secret-operator-deployer
+      app.kubernetes.io/instance: secret-operator-deployer
+      stackable.tech/vendor: Stackable
+  template:
+    metadata:
+      labels:
+        app.kubernetes.io/name: secret-operator-deployer
+        app.kubernetes.io/instance: secret-operator-deployer
+    spec:
+      serviceAccountName: secret-operator-deployer
+      securityContext: {}
+      containers:
+        - name: secret-operator-deployer
+          securityContext: {}
+          image: "quay.io/stackable/tools@sha256:bb02df387d8f614089fe053373f766e21b7a9a1ad04cb3408059014cb0f1388e"
+          imagePullPolicy: IfNotPresent
+          command: ["/usr/bin/bash", "/manifests/deploy.sh"]
+          env:
+            - name: SOME_IMPORTANT_FEATURE_FLAG
+              value: "turn-it-on"
+            - name: OP_VERSION
+              value: '24.11.0'
+            - name: NAMESPACE
+              valueFrom:
+                fieldRef:
+                  fieldPath: metadata.namespace
+          resources:
+            limits:
+              cpu: 100m
+              memory: 512Mi
+            requests:
+              cpu: 100m
+              memory: 512Mi
+          volumeMounts:
+            - name: manifests
+              mountPath: /manifests
+      volumes:
+        - name: manifests
+          configMap:
+            name: secret-operator-deployer-manifests
+      tolerations:
+        - key: keep-out
+          value: "yes"
+          operator: Equal
+          effect: NoSchedule
+    "#;
+
+    #[test]
+    fn test_copy_tolerations() -> Result<()> {
+        let data = serde_yaml::Value::deserialize(serde_yaml::Deserializer::from_str(DEPLOYMENT))?;
+        //let do_deployment: DynamicObject = serde_yaml::from_value(data)?;
+        //let deployment: Deployment =do_deployment.try_parse()?;
+        let deployment: Deployment = serde_yaml::from_value(data)?;
+
+        let data = serde_yaml::Value::deserialize(serde_yaml::Deserializer::from_str(DAEMONSET))?;
+        let daemonset: DynamicObject = serde_yaml::from_value(data)?;
+
+        let daemonset: DaemonSet = maybe_copy_tolerations(&deployment, &daemonset)?.unwrap();
+
+        assert_eq!(
+            daemonset_tolerations(&daemonset),
+            deployment_tolerations(&deployment)
+        );
+
+        Ok(())
+    }
+
+    fn maybe_copy_tolerations(
+        deployment: &Deployment,
+        res: &DynamicObject,
+    ) -> Result<Option<DaemonSet>> {
+        let ds: Result<DaemonSet, _> = res.clone().try_parse();
+        match ds {
+            Ok(mut daemonset) => {
+                if let Some(dts) = deployment_tolerations(deployment) {
+                    if let Some(pod_spec) = daemonset
+                        .spec
+                        .as_mut()
+                        .and_then(|s| s.template.spec.as_mut())
+                    {
+                        match pod_spec.tolerations.as_mut() {
+                            Some(ta) => ta.extend(dts.clone()),
+                            _ => pod_spec.tolerations = Some(dts.clone()),
+                        }
+                    }
+                }
+
+                Ok(Some(daemonset))
+            }
+            _ => Ok(None),
+        }
+    }
+
+    fn deployment_tolerations(deployment: &Deployment) -> Option<&Vec<Toleration>> {
+        deployment
+            .spec
+            .as_ref()
+            .and_then(|s| s.template.spec.as_ref())
+            .and_then(|ps| ps.tolerations.as_ref())
+    }
+
+    fn daemonset_tolerations(res: &DaemonSet) -> Option<&Vec<Toleration>> {
+        res.spec
+            .as_ref()
+            .and_then(|s| s.template.spec.as_ref())
+            .and_then(|ps| ps.tolerations.as_ref())
+    }
 }
