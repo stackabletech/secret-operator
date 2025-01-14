5a3302b248f2b5596a2d6a531db27df437d3c213
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2025-01-14 09:26:08 +0100

    test env

--- rust/olm-deployer/src/env/mod.rs
+++ rust/olm-deployer/src/env/mod.rs
@@ -47,3 +47,102 @@ fn deployment_env_var(deployment: &Deployment) -> Vec<EnvVar> {
         .and_then(|c| c.env.clone())
         .unwrap_or_default()
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use anyhow::Result;
+    use serde::Deserialize;
+
+    use std::sync::LazyLock;
+
+    static DAEMONSET: LazyLock<DynamicObject> = LazyLock::new(|| {
+        const STR_DAEMONSET: &str = r#"
+---
+apiVersion: apps/v1
+kind: DaemonSet
+metadata:
+  name: secret-operator-daemonset
+spec:
+  template:
+    spec:
+      containers:
+        - name: secret-operator
+          image: "quay.io/stackable/secret-operator@sha256:bb5063aa67336465fd3fa80a7c6fd82ac6e30ebe3ffc6dba6ca84c1f1af95bfe"
+          env:
+            - name: NAME1
+              value: value1
+"#;
+
+        let data =
+            serde_yaml::Value::deserialize(serde_yaml::Deserializer::from_str(STR_DAEMONSET))
+                .unwrap();
+        serde_yaml::from_value(data).unwrap()
+    });
+
+    static DEPLOYMENT: LazyLock<Deployment> = LazyLock::new(|| {
+        const STR_DEPLOYMENT: &str = r#"
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: secret-operator-deployer
+  uid: d9287d0a-3069-47c3-8c90-b714dc6d1af5
+spec:
+  template:
+    spec:
+      containers:
+        - name: secret-operator-deployer
+          image: "quay.io/stackable/tools@sha256:bb02df387d8f614089fe053373f766e21b7a9a1ad04cb3408059014cb0f1388e"
+          env:
+            - name: NAME2
+              value: value2
+      tolerations:
+        - key: keep-out
+          value: "yes"
+          operator: Equal
+          effect: NoSchedule
+    "#;
+
+        let data =
+            serde_yaml::Value::deserialize(serde_yaml::Deserializer::from_str(STR_DEPLOYMENT))
+                .unwrap();
+        serde_yaml::from_value(data).unwrap()
+    });
+
+    #[test]
+    fn test_copy_env_var() -> Result<()> {
+        let daemonset = maybe_copy_env(&DEPLOYMENT, DAEMONSET.clone())?;
+
+        let expected = vec![
+            EnvVar {
+                name: "NAME1".to_string(),
+                value: Some("value1".to_string()),
+                ..EnvVar::default()
+            },
+            EnvVar {
+                name: "NAME2".to_string(),
+                value: Some("value2".to_string()),
+                ..EnvVar::default()
+            },
+        ];
+        assert_eq!(
+            daemonset_env(&daemonset.try_parse::<DaemonSet>()?),
+            expected
+        );
+        Ok(())
+    }
+
+    fn daemonset_env(res: &DaemonSet) -> Vec<EnvVar> {
+        res.spec
+            .as_ref()
+            .and_then(|ds| ds.template.spec.as_ref())
+            .map(|ts| ts.containers.iter())
+            .into_iter()
+            .flatten()
+            .filter(|c| c.name == "secret-operator")
+            .last()
+            .and_then(|c| c.env.clone())
+            .unwrap_or_default()
+    }
+}
