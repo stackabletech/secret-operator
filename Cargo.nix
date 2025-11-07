4e9fd1ab8d6bbf95d99020d9826875f5ad3e23f1
Techassi <git@techassi.dev>
2025-11-07 09:56:27 +0100

    chore: Release 25.11.0
    Signed-off-by: Techassi <git@techassi.dev>

--- Cargo.nix
+++ Cargo.nix
@@ -1169,7 +1169,7 @@ rec {
       };
       "cert-tools" = rec {
         crateName = "cert-tools";
-        version = "25.11.0-rc1";
+        version = "25.11.0";
         edition = "2021";
         crateBin = [
           {
@@ -8064,7 +8064,7 @@ rec {
       };
       "p12" = rec {
         crateName = "p12";
-        version = "25.11.0-rc1";
+        version = "25.11.0";
         edition = "2021";
         src = lib.cleanSourceWith { filter = sourceFilter;  src = ./rust/p12; };
         authors = [
@@ -11554,7 +11554,7 @@ rec {
       };
       "stackable-krb5-provision-keytab" = rec {
         crateName = "stackable-krb5-provision-keytab";
-        version = "25.11.0-rc1";
+        version = "25.11.0";
         edition = "2021";
         crateBin = [
           {
@@ -11846,7 +11846,7 @@ rec {
       };
       "stackable-secret-operator" = rec {
         crateName = "stackable-secret-operator";
-        version = "25.11.0-rc1";
+        version = "25.11.0";
         edition = "2021";
         crateBin = [
           {
@@ -12026,7 +12026,7 @@ rec {
       };
       "stackable-secret-operator-olm-deployer" = rec {
         crateName = "stackable-secret-operator-olm-deployer";
-        version = "25.11.0-rc1";
+        version = "25.11.0";
         edition = "2021";
         crateBin = [
           {
@@ -12091,7 +12091,7 @@ rec {
       };
       "stackable-secret-operator-utils" = rec {
         crateName = "stackable-secret-operator-utils";
-        version = "25.11.0-rc1";
+        version = "25.11.0";
         edition = "2021";
         src = lib.cleanSourceWith { filter = sourceFilter;  src = ./rust/utils; };
         libName = "stackable_secret_operator_utils";
